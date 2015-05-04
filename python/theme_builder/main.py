a44fd0fdae4ae560114a3f34f871d0cb66307d1b
Rudity <darrenjrudy@gmail.com>
2015-05-03 21:04:52 -0700

    theme tester update
    update to qtapp for verifying css theme work

--- python/theme_builder/main.py
+++ python/theme_builder/main.py
@@ -26,10 +26,58 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.toolBar.addWidget(QtGui.QPushButton())
         self.toolBar.addWidget(QtGui.QPushButton())
 
+        list_model = QtGui.QStandardItemModel(self.listView)
+        self.letters = ['A', 'B', 'C', 'D', 'E']
+        for letter in self.letters:
+            item = QtGui.QStandardItem(letter)
+            item.setCheckable(True)
+            list_model.appendRow(item)
+
+        self.listView.setModel(list_model)
+        self.listView_2.setModel(list_model)
+        self.treeView.setModel(list_model)
+
+        table_model = MyTableModel(self, DATA_LIST, HEADER)
+        self.tableView.setModel(table_model)
+
+
+class MyTableModel(QtCore.QAbstractTableModel):
+    def __init__(self, parent, mylist, header, *args):
+        super(MyTableModel, self).__init__(parent)
+        self.mylist = mylist
+        self.header = header
+
+    def rowCount(self, parent):
+        return len(self.mylist)
+
+    def columnCount(self, parent):
+        return len(self.mylist[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        elif role != QtCore.Qt.DisplayRole:
+            return None
+        return self.mylist[index.row()][index.column()]
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self.header[col]
+        return None
+
+# the solvent data ...
+HEADER = ['Name', 'Height', 'Weight', 'Reach']
+# use numbers for numeric data to sort properly
+DATA_LIST = [('Dude', 180, 66, 68),
+             ('Guy', 170, 64, 69),
+             ('Friend', 172, 68, 74),
+             ('Buddy', 187, 62, 70)]
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
     tt = MainWindow()
+    tt.move(2040, 40)
     tt.show()
     sys.exit(app.exec_())
 
